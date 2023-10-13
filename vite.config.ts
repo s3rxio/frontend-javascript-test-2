4503e1e160cb4f7486174b5b9a267fff8ebda684
s3rxio <cergotabagua222@gmail.com>
2023-10-14 02:57:58 +0300

    Add Docker support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  preview: {
+    host: "0.0.0.0",
+    port: 8080,
+  }
 });
